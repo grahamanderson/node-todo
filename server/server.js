e9f401be1018aa4155fefac9341ca9fc9fd2de9c
Graham Anderson <grahama@me.com>
2017-06-16 17:14:44 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -52,6 +52,36 @@ app.get('/todos/:id',(req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  var id = req.params.id
+  console.log('id', id)
+  // validate id -> not valid? return 404
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  // remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+  //success
+
+    // if no doc, send 404
+    if(!todo) {
+      res.status(404).send()
+    }
+
+    // if doc, send doc back with a 200
+    res.status(200).send({todo})
+    //error
+    }).catch((e) => {
+      // 400 with empty body
+      res.status(400).send()
+    })
+
+
+
+
+})
 
 
 
