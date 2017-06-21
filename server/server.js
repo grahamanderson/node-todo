68e2d615ba5053e6f0ee25c4bc5462362161b1cc
Graham Anderson <grahama@me.com>
2017-06-21 10:18:16 -0500

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -111,18 +111,9 @@ app.post('/users', (req, res) => {
   })
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-  // res.send(req.user);
-  var token = req.header('x-auth')
 
-  User.findByToken(token).then((user) => {
-    if (!user) {
-        return Promise.reject()
-    }
-    res.send(user)
-  }).catch((e) => {
-    res.status(401).send()
-  })
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 });
 
 app.listen(port, () => {
